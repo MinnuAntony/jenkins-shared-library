4ce847ab064f34a81cca79d2584bd9ac12128c92
Minnu Antony <minnu.antony05@gmail.com>
2025-10-19 23:27:15 +0530

    old one

--- vars/mvnTest.groovy
+++ vars/mvnTest.groovy
@@ -1,18 +1,5 @@
-// def call(){
-//     sh 'mvn test'
-// }
-
-// def call() {
-//     sh '''
-//        export MAVEN_OPTS="--add-opens=jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED"
-//        mvn clean package -DskipTests
-//     '''
-// }
-def call() {
-    sh '''
-                    export MAVEN_OPTS="--add-opens=jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED \
---add-opens=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED \
---add-opens=jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED"
-                    mvn clean package -DskipTests
-                '''
+def call(){
+    sh 'mvn test'
 }
+
+
