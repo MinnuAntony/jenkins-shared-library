ec6f1ee3b4d0f6b295c5cf9ba1cd71e242d43a0a
Minnu Antony <minnu.antony05@gmail.com>
2025-10-19 23:25:03 +0530

    again bypass

--- vars/mvnTest.groovy
+++ vars/mvnTest.groovy
@@ -10,8 +10,9 @@
 // }
 def call() {
     sh '''
-       export MAVEN_OPTS="--add-opens=jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED \
---add-opens=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED"
-       mvn clean package -DskipTests
-    '''
+                    export MAVEN_OPTS="--add-opens=jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED \
+--add-opens=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED \
+--add-opens=jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED"
+                    mvn clean package -DskipTests
+                '''
 }
