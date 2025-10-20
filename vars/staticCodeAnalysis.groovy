b7862d4bc42d70abb99e50491f0b49d36200bfb2
Minnu Antony <minnu.antony05@gmail.com>
2025-10-20 13:57:21 +0530

    refactor static code analysis script to use fixed credentials and improved command structure

--- vars/staticCodeAnalysis.groovy
+++ vars/staticCodeAnalysis.groovy
@@ -1,7 +1,22 @@
 
-def call(credentialsId){
+// def call(credentialsId){
 
-    withSonarQubeEnv(credentialsId: credentialsId) {
-         sh 'mvn clean package sonar:sonar'
+//     withSonarQubeEnv(credentialsId: credentialsId) {
+//          sh 'mvn clean package sonar:sonar'
+//     }
+// }
+
+def call() {
+    withSonarQubeEnv('sonar-api') {  // 'sonarqube' is the name configured in Jenkins global tool config
+        sh '''
+            echo "Running SonarQube Static Code Analysis..."
+            mvn clean verify sonar:sonar \
+                -Dsonar.projectKey=kubernetes-configmap-reload \
+                -Dsonar.projectName="SpringBoot Sample" \
+                -Dsonar.host.url=$SONAR_HOST_URL \
+                -Dsonar.login=$SONAR_AUTH_TOKEN
+        '''
     }
+
+    
 }
