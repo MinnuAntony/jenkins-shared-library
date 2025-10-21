2f2727a885b0efff86004491b834613b646ac2ab
MinnuAntony <minnu.antony05@gmail.com>
2025-10-21 18:01:58 +0530

    add docker image push functionality

---
+++ vars/dockerImagePush.groovy
@@ -0,0 +1,11 @@
+def call(String project, String imageTag, String hubUser){
+    withCredentials([usernamePassword(credentialsId: 'dockerhub',
+                     usernameVariable: 'DOCKER_HUB_USER', 
+                     passwordVariable: 'DOCKER_HUB_PASS')]) {
+        sh """
+        docker login -u '$DOCKER_HUB_USER' -p '$DOCKER_HUB_PASS'
+        docker push ${hubUser}/${project}:${imageTag}
+        docker push ${hubUser}/${project}:latest
+        """
+    }}
+
