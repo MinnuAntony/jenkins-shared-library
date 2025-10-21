469afc5d366e009838598a2ec8012c192a1400c8
MinnuAntony <minnu.antony05@gmail.com>
2025-10-21 12:31:38 +0530

    clean

--- vars/staticCodeAnalysis.groovy
+++ vars/staticCodeAnalysis.groovy
@@ -6,18 +6,5 @@ def call(credentialsId){
     }
 }
 
-// def call() {
-//     withSonarQubeEnv('sonar-api') {  // 'sonarqube' is the name configured in Jenkins global tool config
-//         sh '''
-//             echo "Running SonarQube Static Code Analysis..."
-//             mvn clean verify -DskipTests sonar:sonar \
-//                 -Dsonar.projectKey=kubernetes-configmap-reload \
-//                 -Dsonar.projectName="SpringBoot Sample" \
-//                 -Dsonar.host.url=$SONAR_HOST_URL \
-//                 -Dsonar.login=$SONAR_AUTH_TOKEN
-//         '''
-//     }
 
-    
-// }
 
