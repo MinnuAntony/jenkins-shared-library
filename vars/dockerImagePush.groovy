01babacfc4c33d07ddb4ae62236abfa4fbb95f9a
MinnuAntony <minnu.antony05@gmail.com>
2025-10-22 13:25:25 +0530

    refactor: simplify AWS ECR push function by removing credential handling

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -17,19 +17,12 @@
 //     """
 // }
 
-def call(String aws_account_id, String region, String ecr_repoName) {
-    withCredentials([usernamePassword(credentialsId: 'aws-ecr-credentials', usernameVariable: 'AWS_ACCESS_KEY_ID', passwordVariable: 'AWS_SECRET_ACCESS_KEY')]) {
-        sh """
-            # Configure AWS CLI with Jenkins credentials
-            aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-            aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-            aws configure set default.region ${region}
 
-            # Authenticate Docker to AWS ECR
-            aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
 
-            # Push the Docker image
-            docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
-        """
-    }
+def call(String aws_account_id, String region, String ecr_repoName){
+    
+    sh """
+     aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+     docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+    """
 }
