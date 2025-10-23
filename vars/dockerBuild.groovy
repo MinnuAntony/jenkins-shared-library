c92398138706eb89abf2be4d30d8203da7ee898a
MinnuAntony <minnu.antony05@gmail.com>
2025-10-23 15:08:20 +0530

    docker hub uncommented

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,17 +1,17 @@
-// def call(String project, String imageTag, String hubUser){
-//     sh """
-//     docker build -t ${hubUser}/${project} .
-//     docker tag ${hubUser}/${project} ${hubUser}/${project}:${imageTag}
-//     docker tag ${hubUser}/${project} ${hubUser}/${project}:latest
-
-//     """
-// }
-
-
-def call(String aws_account_id, String region, String ecr_repoName){
-    
+def call(String project, String imageTag, String hubUser){
     sh """
-     docker build -t ${ecr_repoName} .
-     docker tag ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:v1
+    docker build -t ${hubUser}/${project} .
+    docker tag ${hubUser}/${project} ${hubUser}/${project}:${imageTag}
+    docker tag ${hubUser}/${project} ${hubUser}/${project}:latest
+
     """
 }
+
+
+// def call(String aws_account_id, String region, String ecr_repoName){
+    
+//     sh """
+//      docker build -t ${ecr_repoName} .
+//      docker tag ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:v1
+//     """
+// }
